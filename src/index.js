3e6b18b7aac5fb86135a15af5983f353b9f6a773
anujdarji2001 <anujdarji2001@gmail.com>
2021-07-25 14:51:16 +0530

    Some Change

--- src/index.js
+++ src/index.js
@@ -3,27 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-// ReactDOM.render(
+
+ReactDOM.render(
   
-//     <App />,
-  
-//   document.getElementById('root')
-// );
+    <App />,
+  document.getElementById('root')
+);
 
-const loader = document.querySelector('.loader');
 
-// if you want to show the loader when React loads data again
-const showLoader = () => loader.classList.remove('loader--hide');
-
-const hideLoader = () => loader.classList.add('loader--hide');
-
-setTimeout(() => 
-  // the show/hide functions are passed as props
-  ReactDOM.render(
-    <App
-      hideLoader={hideLoader}
-      showLoader={showLoader} 
-      />,
-    document.getElementById('root')
-  )
-, 2000);
